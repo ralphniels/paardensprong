4e9e86a6c07634f3be36e04c98219e716f8fc6ab
Ralph Niels <ralph.niels@han.nl>
2026-09-23 15:47:16 +0200

    Sessie alleen starten als die door inloggen niet al gestart is.

--- index.php
+++ index.php
@@ -5,12 +5,13 @@ declare(strict_types=1);
 if ($_SERVER['SERVER_NAME'] == 'localhost') {
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
+    session_start();
+
 } else {
     require_once("../inloggen/inloggen.php");
 }
 
 
-session_start();
 
 if (!isset($_SESSION['csrf_token'])) {
     $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
